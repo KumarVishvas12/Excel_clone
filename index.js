c4a0c3f633baaa61a5a241cd8a3d501d8f96a3c8
Your Name <you@example.com>
2023-10-12 23:41:50 +0530

    added multi cell selection , bold, italic, underline functionality on buttns etc

--- index.js
+++ index.js
@@ -55,22 +55,10 @@ for (let j = 1; j<= 52; j++) {
 
 diva = document.createElement("div")
 
-
-// while (n > 0) {
-//     let rem = n % 26;
-//     if (rem == 0) {
-//         ans = "Z" + ans;
-//         n = Math.floor(n / 26) - 1;
-//     } else {
-//         ans = String.fromCharCode(rem - 1 + 65) + ans;
-//         n = Math.floor(n / 26);
-//     }
-// }
 diva.id = "rowid-"+i+"-colid-"+j;
 diva.className="input-cell";
-// diva.contentEditable="true"
+
 cell_row.appendChild(diva)
-// document.getElementById(ans).contentEditable="true";
 }
 cell_row_container.appendChild(cell_row);
 }
@@ -97,28 +85,79 @@ document.getElementById('rowid-1-colid-1').classList.add('selected');
 
 document.querySelectorAll('.input-cell').forEach((i)=>{i.addEventListener('click',(e)=>{
     // console.log(e);
+    
+    let prevcell;
     if(e.ctrlKey){
         idarray=i.id.split("-");
-        let [rowId,colId]=[idarray[1],idarray[3]]
-        if(rowId>1){
-        let top_cell_selected=document.getElementById(`rowid-${rowId-1}-colid-${colId}`).classList.contains("selected");
-        if(top_cell_selected){
-            i.classList.add("top-cell-selected");
-            i.classList.add("selected")
-            document.getElementById(`rowid-${rowId-1}-colid-${colId}`).classList.add('bottom-cell-selected')
-        }
-        }
+        let [rowId,colId]=[Math.floor(idarray[1]),Math.floor(idarray[3])]
+       
+        // let [prevrowId,prevcolId]=[Math.floor(prevcell[1]),Math.floor(prevcell[3])]
+        // prevcell=i.id.split("-");
+        // let [prevrowId,prevcolId]=[Math.floor(prevcell[1]),Math.floor(prevcell[3])]
+        // console.log(prevcell)
+
+        // for(let i=prevrowId;i<=Math.abs(prevrowId-rowId);i++){
+        //     document.querySelector(`rowid-${i}-colid-${prevcolId}`).classList.add("selected")
+        // }
+
+        i.classList.add("selected")
+
+        // if(rowId>1){
+        // let top_cell_selected=document.getElementById(`rowid-${rowId-1}-colid-${colId}`).classList.contains("selected");
+        // if(top_cell_selected){
+        //     i.classList.add("top-cell-selected");
+        //     i.classList.add("selected")
+        //     document.getElementById(`rowid-${rowId-1}-colid-${colId}`).classList.add('bottom-cell-selected')
+        // }
+        // }
+        // if(rowId<row_number){
+        //     let bottom_cell_selected=document.getElementById(`rowid-${rowId+1}-colid-${colId}`).classList.contains("selected");
+            
+        //     if(bottom_cell_selected){
+        //         i.classList.add("bottom-cell-selected");
+        //         i.classList.add("selected")
+        //         document.getElementById(`rowid-${Math.floor(rowId)+1}-colid-${colId}`).classList.add('top-cell-selected')
+        //     }
+        //     }
+
+
+
+        //     if(colId>1){
+        //         let left_cell_selected=document.getElementById(`rowid-${rowId}-colid-${colId-1}`).classList.contains("selected");
+        //         if(left_cell_selected){
+        //             i.classList.add("left-cell-selected");
+        //             i.classList.add("selected")
+        //             document.getElementById(`rowid-${rowId}-colid-${colId-1}`).classList.add('right-cell-selected')
+        //         }
+        //         }
+
+
+
+
+        //         if(colId<row_number){
+        //             let right_cell_selected=document.getElementById(`rowid-${rowId}-colid-${colId+1}`).classList.contains("selected");
+                    
+        //             if(right_cell_selected){
+        //                 i.classList.add("right-cell-selected");
+        //                 i.classList.add("selected")
+        //                 document.getElementById(`rowid-${rowId}-colid-${colId+1}`).classList.add('left-cell-selected')
+        //             }
+        //             }
         
         
     }
     else{
-    document.querySelector('.input-cell.selected').classList.remove('selected')
+        
+    // prevcell=document.querySelector('.input-cell.selected').id.split("-")
+    // console.log(prevcell)
+    document.querySelectorAll('.input-cell.selected').forEach((c)=>{c.classList.remove('selected')})
     i.classList.add('selected')   }
 })})
 
 //Applying double click property in eacg cell to make it editable
 document.querySelectorAll('.input-cell').forEach((e)=>{e.addEventListener("dblclick",()=>{
     document.querySelector('.input-cell.selected').classList.remove('selected')
+    document.querySelectorAll('.input-cell').forEach((i)=>{i.setAttribute("contentEditable","false")})
     e.classList.add('selected')
     e.setAttribute("contentEditable", "true")  
 })})
@@ -132,6 +171,49 @@ scroller.addEventListener("scroll",(e)=>{
 
 
 
+//adding bold functionality
+document.querySelector("#bold").addEventListener("click",()=>{
+    document.querySelectorAll(".selected").forEach((i)=>{
+        if(i.classList.contains("bold")){
+            i.classList.remove("bold")
+        }
+        else{ i.classList.add("bold")}
+       
+    })
+})
+
+//adding Italic funcationality
+document.querySelector("#italic").addEventListener("click",()=>{
+    document.querySelectorAll(".selected").forEach((i)=>{
+        if(i.classList.contains("italic")){i.classList.remove("italic")}
+        else{ i.classList.add("italic")}
+        
+    })
+})
+
+//Adding underlined functionality
+document.querySelector("#underline").addEventListener("click",()=>{
+    document.querySelectorAll(".selected").forEach((i)=>{
+        if(i.classList.contains("underline")){i.classList.remove("underline")}
+        else{ i.classList.add("underline")}
+        
+        
+    })
+})
+
+
+document.querySelector("#color-fill").addEventListener("click",()=>{
+    document.querySelectorAll(".selected").forEach((i)=>{
+        console.log("vishvas")
+        if(i.classList.contains("fill")){i.classList.remove("fill") 
+        i.classList.remove("selected")}
+        else{ i.classList.add("fill")
+        i.classList.remove("selected")
+    }
+        
+        
+    })
+})
 
 
 
